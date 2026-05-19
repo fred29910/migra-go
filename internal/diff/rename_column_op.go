60dc1c2da4139e605cb1b816718ec53e2d2e5a29
fred29910 <vito86077@gmail.com>
2026-05-19 13:54:43 +0800

    feat: add KindRenameColumn and RenameColumnOp

---
+++ internal/diff/rename_column_op.go
@@ -0,0 +1,33 @@
+package diff
+
+import "github.com/fred29910/migra-go/internal/model"
+
+// RenameColumnOp represents renaming a column.
+type RenameColumnOp struct {
+	baseOperation
+	Schema  string
+	Table   string
+	OldName string
+	NewName string
+}
+
+func NewRenameColumnOp(schema, table, oldName, newName string) *RenameColumnOp {
+	return &RenameColumnOp{
+		baseOperation: baseOperation{
+			kind:      KindRenameColumn,
+			objectKey: model.NewObjectKey(schema, table+"."+oldName, model.KindColumn),
+		},
+		Schema:  schema,
+		Table:   table,
+		OldName: oldName,
+		NewName: newName,
+	}
+}
+
+func (op *RenameColumnOp) IsDestructive() bool { return false }
+
+func (op *RenameColumnOp) DependsOn() []model.ObjectKey {
+	return []model.ObjectKey{
+		model.NewObjectKey(op.Schema, op.Table, model.KindTable),
+	}
+}
